80030381cb2ca96e73927ffc34289b2f566bcd52
cohyou <cohyou@ironoir.io>
2024-06-30 12:00:26 +0900

    first commit

---
+++ src/parser.rs
@@ -0,0 +1,17 @@
+use nom::IResult;
+use nom::number::complete::be_u16;
+use nom::bytes::complete::take;
+
+pub fn length_value(input: &[u8]) -> IResult<&[u8],&[u8]> {
+    let (input, length) = be_u16(input)?;
+    take(length)(input)
+}
+
+pub fn schema(input: &str) -> IResult<&str, &str> {
+    unimplemented!()
+}
+
+#[test]
+fn test() {
+    assert!("wowow".is_ascii());
+}
